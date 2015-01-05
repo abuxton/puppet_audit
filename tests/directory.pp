471d94017ead402ed50330f62707806d97b99800
Adam Buxton <buxton.adam@gmail.com>
2015-01-05 12:39:58 -0800

    update for tests, need to update params for test data

---
+++ tests/directory.pp
@@ -0,0 +1,19 @@
+# The baseline for module testing used by Puppet Labs is that each manifest
+# should have a corresponding test manifest that declares that class or defined
+# type.
+#
+# Tests are then run by using puppet apply --noop (to check for compilation
+# errors and view a log of events) or by fully applying the test in a virtual
+# environment (to compare the resulting system state to the desired state).
+#
+# Learn more about module testing here:
+# http://docs.puppetlabs.com/guides/tests_smoke.html
+#
+include puppet_audit
+puppet_audit::directory { "${puppet_audit::params::dirpath}":
+dirMD5 => "${puppet_audit::params::dirMD5}"
+$tags  => "${puppet_audit::params::tags}"
+
+  
+}
+
