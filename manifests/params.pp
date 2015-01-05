e6e93695d3d8ededdd3a4306f9f6748b0a658d6e
Adam Buxton <buxton.adam@gmail.com>
2015-01-05 13:26:50 -0800

    test params

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,11 +4,25 @@
 # It sets variables according to platform
 #
 class puppet_audit::params {
+  case $operatingsystem {
+  "Darwin": {
+  $filepath = "/tmp/afile.txt"
+  $dirpath  = "/tmp"
+#md5 /tmp/afile.txt
+#MD5 (/tmp/afile.txt) = d41d8cd98f00b204e9800998ecf8427e
+#$ tree /tmp/
+#/tmp/
+#└── afile.txt
+  $fileMD5  = '{md5}d41d8cd98f00b204e9800998ecf8427e' 
+  $dirMD5   = undef
+  $tags     = undef
+  }
+  default: {
   $filepath = undef
   $dirpath  = undef
   $fileMD5  = undef
   $dirMD5   = undef
   $tags     = undef
-  
+  }
 }
 
