7c4f9980e8b8d51811d4334423e943da4d6d9576
Adam Buxton <buxton.adam@gmail.com>
2018-10-18 13:59:35 +0100

    ?

--- examples/file.pp
+++ examples/file.pp
@@ -10,13 +10,14 @@
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
 include puppet_audit
-#notify {"${puppet_audit::params::filepath}":}
-#notify {"${puppet_audit::params::fileMD5}":}
-puppet_audit::file { $puppet_audit::params::filepath:
-fileMD5 => $puppet_audit::params::fileMD5,
-group   => $puppet_audit::params::group,
-owner   => $puppet_audit::params::owner,
-mode    => $puppet_audit::params::mode,
-tags    => $puppet_audit::params::tags,
+
+puppet_audit::file { '/tmp/puppet_audit_test':
+checksum        => $puppet_audit::params::checksum,
+checksum_value  => $puppet_audit::params::checksum_value,
+content         => $puppet_audit::params::content,
+group           => $puppet_audit::params::group,
+owner           => $puppet_audit::params::owner,
+mode            => $puppet_audit::params::mode,
+tags            => $puppet_audit::params::tags,
 }
 
